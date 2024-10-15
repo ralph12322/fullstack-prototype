a78802b9376199e2cd1066c9fc9a82437d85d2ba
ralph12322 <gjcshs.santos.ralphgeo@gmail.com>
2024-10-15 20:38:49 +0800

    first

---
+++ backend/script.js
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    name: 'shoes',
+    size: [
+      '8',
+      '9',
+      '10'
+    ],
+    prices: [
+      5000,
+      5500,
+      6500
+    ]
+  },
+  {
+    name: 'socks',
+    size: [
+      'S',
+      'M',
+      'L'
+    ],
+    prices: [
+      400,
+      500,
+      600
+    ]
+  }
+]
